75249c05c0c0d1b6cf3f5a37bb5ea9e42659890d
AMATEURTOSS <amateur.toss@gmail.com>
2024-10-15 09:08:15 +0900

    chore: mdx 설정 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
+import nextMdx from '@next/mdx'
+
+const withMdx = nextMdx()
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   distDir: 'dist',
-};
+  pageExtensions: ['tsx', 'mdx'],
+}
 
-export default nextConfig;
+export default withMdx(nextConfig)
