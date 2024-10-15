d4073735b4804881904846bd7d27e45016b6c09d
AMATEURTOSS <amateur.toss@gmail.com>
2024-10-15 14:24:07 +0900

    update: 홈 디자인 단순화

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,23 +4,21 @@ import { Post } from '@/components/post'
 
 export default function Home() {
   return (
-    <div className='container flex flex-col items-center min-h-screen mx-auto py-8'>
+    <div className='prose container flex flex-col items-center min-h-screen mx-auto py-8'>
       <header className='flex items-center gap-2 font-medium'>
-        <Link className='underline underline-offset-4' href='https://github.com/AMATEURTOSS' target='_blank' rel='noreferrer'>
+        <Link href='https://github.com/AMATEURTOSS' target='_blank' rel='noreferrer'>
           blog
         </Link>
         {'/'}
-        <Link className='underline underline-offset-4' href='https://github.com/AMATEURTOSS' target='_blank' rel='noreferrer'>
+        <Link href='https://github.com/AMATEURTOSS' target='_blank' rel='noreferrer'>
           github
         </Link>
       </header>
-      <main className='flex-1 px-8'>
-        <div className='container flex flex-col space-y-2'>
-          <article className='pt-6'>
-            <h2 className='text-lg tracking-tighter'>
-              posts
-            </h2>
-            <ul className='space-y-4 py-4'>
+      <main>
+        <div>
+          <article>
+            <h2>posts</h2>
+            <ul>
               {
                 posts.map((post) => (
                   <Post key={post.href} {...post} />
--- src/components/post.tsx
+++ src/components/post.tsx
@@ -1,17 +1,18 @@
 import type { Post } from '@/db'
+import Link from 'next/link'
 
 export function Post ({ title, description = '', createdAt, href = '/' }: Post) {
   return (
     <li className='grid items-start gap-1'>
-      <a className='flex items-center gap-1.5' href={href}>
+      <Link className='flex items-center gap-1.5 no-underline' href={href}>
         <span className='font-medium underline underline-offset-4'>
           {title}
         </span>
         <span>{'-'}</span>
-        <span className='text-xs tracking-tight text-foreground underline'>
+        <span className='text-xs tracking-tight text-foreground'>
           {createdAt}
         </span>
-      </a>
+      </Link>
       <span className='text-sm text-foreground'>
         {description}
       </span>
