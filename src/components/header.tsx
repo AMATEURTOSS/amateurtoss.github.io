5533a53c012c4ac825454c26c83fa55ef53a8da7
AMATEURTOSS <amateur.toss@gmail.com>
2024-10-18 08:18:01 +0900

    update: 링크 새창 제거

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,15 +3,15 @@ import Link from 'next/link'
 export function Header () {
   return (
     <header className='flex items-center gap-2 font-medium'>
-      <Link href='https://github.com/AMATEURTOSS' target='_blank' rel='noreferrer'>
+      <Link href='https://github.com/AMATEURTOSS'>
         blog
       </Link>
       {'/'}
-      <Link href='https://github.com/AMATEURTOSS' target='_blank' rel='noreferrer'>
+      <Link href='https://github.com/AMATEURTOSS'>
         github
       </Link>
       {'/'}
-      <Link href='/portfolios' target='_blank' rel='noreferrer'>
+      <Link href='/portfolios'>
         portfolio
       </Link>
     </header>
