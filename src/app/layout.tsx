29fdd4f05cd122e5dac81f261f3fe29897926c11
yochoi <amateur.toss@gmail.com>
2024-11-16 22:22:40 +0900

    update: 블로그 이름, 설명 작성

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,12 @@ import type { PropsWithChildren } from 'react'
 import type { Metadata } from 'next'
 import './globals.css'
 
+const title = '최영진의 개발 노트'
+const description = '웹 개발의 A to Z를 탐구하는 기술 블로그. 실전에서 마주한 문제들과 그 해결 과정을 공유합니다.'
 export const metadata: Metadata = {
-  title: 'Blog',
+  title,
+  description,
+  openGraph: { title, description },
   verification: {
     google: 'OwHKPNKdBwgpiIeHCPodU73heHdBhAoM7A9AphpQxbI'
   },
