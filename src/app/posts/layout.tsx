02a90199a4845c325006ede0ab7e69557b2aacce
yochoi <amateur.toss@gmail.com>
2025-04-11 12:51:59 +0900

    feat: 게시글에 돌아가기 버튼 추가

--- src/app/posts/layout.tsx
+++ src/app/posts/layout.tsx
@@ -1,8 +1,10 @@
 import type { PropsWithChildren } from 'react'
+import Link from 'next/link'
 
 export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <article className='prose container min-h-screen mx-auto p-8'>
+      <Link href='/' className='text-xs'>{'<'} 돌아가기</Link>
       {children}
     </article>
   )
